a107d8b56acc795901c1ddbc340aa085d28c8e49
Patrick Dolan <dolanp1992@gmail.com>
2023-11-29 16:32:17 -0800

    configure password rules for user register for development

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -26,6 +26,31 @@ namespace ToDoList
         .AddEntityFrameworkStores<ToDoListContext>()
         .AddDefaultTokenProviders();
 
+        // ======== UNCOMMENT THE BELOW CODE FOR PRODUCTION ========
+        // Default Password settings for production
+        // builder.Services.Configure<IdentityOptions>(options =>
+        // {
+        //   // Default Password settings.
+        //   options.Password.RequireDigit = true;
+        //   options.Password.RequireLowercase = true;
+        //   options.Password.RequireNonAlphanumeric = true;
+        //   options.Password.RequireUppercase = true;
+        //   options.Password.RequiredLength = 6;
+        //   options.Password.RequiredUniqueChars = 1;
+        // });
+
+        // ======== COMMENT OUT THE BELOW CODE FOR PRODUCTION ========
+        // Default Password settings for development 
+        builder.Services.Configure<IdentityOptions>(options =>
+        {
+          options.Password.RequireDigit = false;
+          options.Password.RequireLowercase = false;
+          options.Password.RequireNonAlphanumeric = false;
+          options.Password.RequireUppercase = false;
+          options.Password.RequiredLength = 0;
+          options.Password.RequiredUniqueChars = 0;
+        });
+
       WebApplication app = builder.Build();
 
       app.UseHttpsRedirection();
--- ToDoList/ViewModels/RegisterViewModel.cs
+++ ToDoList/ViewModels/RegisterViewModel.cs
@@ -9,8 +9,11 @@ namespace ToDoList.ViewModels
     [Display(Name = "Email Address")]
     public string Email { get; set; }
 
+    // Commented out for development purposes
+    // Regex from this StackOverflow answer: https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
     [Required]
     [DataType(DataType.Password)]
+    // [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$", ErrorMessage = "Your password must contain at least six characters, a capital letter, a lowercase letter, a number, and a special character.")]
     public string Password { get; set; }
 
     [Required]
